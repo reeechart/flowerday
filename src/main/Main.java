8504c9b38faa2c2dbfb81b204b177f427d36eeae
Irene Edria <ireneedriadr@gmail.com>
2017-04-23 12:14:32 +0700

    add main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,13 @@
 package main;
 
-import gui.DisplayMenu;
-
-import javax.swing.*;
+import gui.Menu;
 
 /**
  * Created by ireneedriadr on 4/19/17.
  */
 public class Main {
   public static void main(String a[]) {
-    new DisplayMenu();
+    new Menu();
 
 
   }
