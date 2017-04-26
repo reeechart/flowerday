94a8e2a6a3993840977da75ec1afb0445ff2c39a
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-04-26 07:45:53 +0700

    Load dari file

--- src/gui/Load.java
+++ src/gui/Load.java
@@ -1,11 +1,20 @@
 package gui;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * Created by ireneedriadr on 4/26/17.
  */
 public class Load {
+  private String[] name = new String[3];
+  private int[] maxLv = new int[3];
+  private int[] money = new int[3];
+  private int[] truckLv = new int[3];
 
   public Load(JDesktopPane dp) {
     ImageIcon loadbackground = new ImageIcon("asset/gameback.png");
@@ -35,5 +44,30 @@ public class Load {
     loadfile3.setBorderPainted(false);
     loadfile3.setContentAreaFilled(false);
     dp.add(loadfile3, new Integer(550));
+  }
+
+  public void LoadFile() {
+    try(BufferedReader br = new BufferedReader(new FileReader("asset/player.txt"))) {
+      String currentLine = null;
+      int counter = 0;
+      String pattern = "\\s*name\\s*:\\s*([^,]+)\\s*,\\s*maxLv\\s*:\\s*(\\d+)\\s*,\\s*money\\s*:\\s*(\\d+)\\s*,\\s*truckLv\\s*:\\s*(\\d+)\\s*";
+
+      while((currentLine = br.readLine()) != null) {
+         Pattern p = Pattern.compile(pattern);
+         Matcher m = p.matcher(currentLine);
+
+         if(m.find()) {
+           name[counter] = m.group(1);
+           maxLv[counter] = Integer.parseInt(m.group(2));
+           money[counter] = Integer.parseInt(m.group(3));
+           truckLv[counter] = Integer.parseInt(m.group(4));
+         }
+
+        counter++;
+      }
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
   }
 }
